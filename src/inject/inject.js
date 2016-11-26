c7878ab4f72848dd43b7aee8cf842a9dfc10b271
Rohan Verma <rohanverma2004@gmail.com>
2016-11-26 17:57:38 +0530

    feat: added current git repo url

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,32 +2,16 @@ $form = "<p>Login below</p><form style='display: inline;' action='https://diohac
 	"Name: <input type='text' name='name' class='form-control' style='display: inline;'><br>" +
 	"Region: <input type='text' name='region' class='form-control' style='display: inline;'><br>" +
 	"Size: <input type='text' name='size' class='form-control' style='display: inline;'><br>" +
-	"authtoken <input class='form-control' type='text' name='token'>"+
+	"<input type='hidden' name='giturl' id='giturl'><br>" +
+	"authtoken: <input class='form-control' type='text' name='token'><br>"+
 	"<button id='submit' type='submit'>Deploy</button><br>"+
 	"</form>";
 
-	$(function(){
-		console.log('test');
-	});
-
-
-	function update_link(){
-		  var client_id = document.getElementbyId('client_id');
-		  var callback_uri = document.getElementbyId('client_id');
-		  //TODO: Add check before hand
-
-			var final_href = "https://cloud.digitalocean.com/v1/oauth/authorize?response_type=token&client_id="+
-					client_id +
-		  "&redirect_uri=" + callback_uri +
-		  "&scope=read%20write&state=0807edf7d85e5d"
-
-			document.getElementbyId('link_for_login').href = final_href;
-	}
-
 
 function insertBadge(){
-	console.log("Insert Badge!");
+	console.log("Inserting Badge!");
 	$("#readme").prepend("<div class='depoly-form header header-logged-in'><a href=\"#\">deploy to heroku</a><div>"+$form+"</div>");
+	$("#giturl").val(window.location + '.git');
 }
 
 
