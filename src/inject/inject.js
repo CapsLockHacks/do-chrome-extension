540f5c3a1e00e8fd47b2f6670f2f31eb3b2305b3
Rohan Verma <rohanverma2004@gmail.com>
2016-11-26 17:32:14 +0530

    feat: added form to submit and create droplet

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,8 +1,29 @@
-$form = "<form style='display: inline;'>" +
-	"Name: <br><input type='text' name='name' class='form-control' style='display: inline;'><br>" +
-	"Region: <br><input type='text' name='region' class='form-control' style='display: inline;'><br>" +
-	"Size: <br><input type='text' name='size' class='form-control' style='display: inline;'>" +
+$form = "<p>Login below</p><form style='display: inline;' action='https://diohack.herokuapp.com/create'>" +
+	"Name: <input type='text' name='name' class='form-control' style='display: inline;'><br>" +
+	"Region: <input type='text' name='region' class='form-control' style='display: inline;'><br>" +
+	"Size: <input type='text' name='size' class='form-control' style='display: inline;'><br>" +
+	"authtoken <input class='form-control' type='text' name='token'>"+
+	"<button id='submit' type='submit'>Deploy</button><br>"+
 	"</form>";
+
+	$(function(){
+		console.log('test');
+	});
+
+
+	function update_link(){
+		  var client_id = document.getElementbyId('client_id');
+		  var callback_uri = document.getElementbyId('client_id');
+		  //TODO: Add check before hand
+
+			var final_href = "https://cloud.digitalocean.com/v1/oauth/authorize?response_type=token&client_id="+
+					client_id +
+		  "&redirect_uri=" + callback_uri +
+		  "&scope=read%20write&state=0807edf7d85e5d"
+
+			document.getElementbyId('link_for_login').href = final_href;
+	}
+
 
 function insertBadge(){
 	console.log("Insert Badge!");
