f2afbbaf0a530695019edce926dfbe8ea5142ee2
Rohan Verma <rohanverma2004@gmail.com>
2016-11-26 13:49:41 +0530

    feat: check if dockerfile exists and inject badge

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,3 +1,8 @@
+function insertBadge(){
+	console.log("Insert Badge!");
+}
+
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -7,6 +12,22 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
+
+		var links = [];
+		$('a').each(function() {
+		   var href = this.href;
+		   // now process with the href as you wish      
+
+		   if(href.indexOf("Dockerfile")!=-1){
+
+		   	links.push(href);
+
+		 	 }
+		});
+
+		if(links.length >= 1){
+			insertBadge();
+		}
 
 	}
 	}, 10);
